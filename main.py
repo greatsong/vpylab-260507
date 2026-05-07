d860609cfcd5f2ede297d7604ecd4b7f66a7832e
송석리(Song Sukree) <greatsong21@gmail.com>
2026-05-07 15:56:07 +0900

    10,10,0
    — 송석리(Song Sukree) via VPyLab

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vpython import *
 
 # 구를 만들고 움직여보세요
 ball = sphere(pos=vector(0, 0, 0), radius=0.5, color=color.cyan)
-ball.velocity = vector(10, 0, 0)
+ball.velocity = vector(10, 10, 0)
 
 dt = 0.01
 while True:
